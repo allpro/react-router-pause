b7e311793ffe6bb5fb81db23a9c7ff15d9240bc9
Kevin Dalman <development@allpro.net>
2019-04-11 23:52:53 -0700

    Add a dummy test so build will complete without errors

---
+++ tests/index.test.js
@@ -0,0 +1,30 @@
+import expect from 'expect'
+// import React from 'react'
+// import { render, unmountComponentAtNode } from 'react-dom'
+
+
+
+// Class mock
+/* HOLD
+let state = {}
+const setState = function(nextState) {
+	state = nextState
+	return Promise.resolve(state)
+}
+const mockClass = { state, setState }
+*/
+
+
+describe('ReactRouterPause', () => {
+	it('init-test-file', () => {
+		expect(111).toBe(111)
+	})
+
+	/* Sample
+	 it( 'displays a welcome message', () => {
+	 render(<Component/>, node, () => {
+	 expect( node.innerHTML ).toContain( 'Welcome to React components' )
+	 })
+	 })
+	 */
+})
